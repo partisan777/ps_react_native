17e1d7b7cd49ab1ca36b6f5a5a7d47a8d3a0e64e
guschinnn <n173@bk.ru>
2025-03-23 22:58:15 -0400

    HomeWork-1

--- App.tsx
+++ App.tsx
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import { CustomButton } from './common/CustomButton';
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground source={require('./assets/cup.png')} resizeMode="cover" style={styles.backGrounImage}>
+          <Text style={styles.h1}>{`Одно из самых вкусных кофе в городе!`}</Text>
+      </ImageBackground>
+      <Text style={styles.h3}>{`Свежие зёрна, настоящая арабика и бережная обжарка`}</Text>
+      <CustomButton  title={'Начать'} viewStyle={styles.button} textStyle={styles.buttonText}/>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
+    paddingBottom: 28
+  }, h1: {
+    color: '#fff',
+    fontSize: 34,
+    textAlign: 'center',
+    paddingBottom: 21,
+    paddingLeft: 28,
+    paddingRight: 28,
+  }, h3: {
+    color: '#A9A9A9',
+    fontSize: 16,
+    paddingBottom: 24,
+    paddingLeft: 28,
+    paddingRight: 28,
+    textAlign: 'center',
+  }, button: {
+    width: 315,
+    height: 62,
+    borderRadius: 16,
+    paddingTop: 20,
+    paddingRight: 109,
+    paddingBottom: 20,
+    paddingLeft: 109,
+    backgroundColor: '#C67C4E',
+  }, buttonText: {
+    textAlign: 'center',
+    fontSize: 16,
+    color: '#fff',
+  },
+  backGrounImage: {
+    width: '100%',
+    height: '85%',
+    flex: 1,
+    justifyContent: 'flex-end',
   }
 });
